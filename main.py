9dea113b4cad195fa9528e5a53fe4530216f07e0
FlederJ <96187657+FlederJ@users.noreply.github.com>
2023-06-26 09:39:51 +0000

    started pygame loop and defined a class for a tennis ball

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import pygame, sys
-from pygame.locals import QUIT
+import pygame
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+WIDTH = 800
+HEIGHT = 600
+BACKGROUND = (0, 0, 0)
+
+
+class Ball:
+
+    def __init__(self):
+        self.image = pygame.image.load("small_tennis.png")
+        self.rect = self.image.get_rect()
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    ball = Ball()
+
+    while True:
+        screen.fill(BACKGROUND)
+        screen.blit(ball.image, ball.rect)
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
